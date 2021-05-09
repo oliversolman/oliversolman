261a7b0f8a83197902f23344efabca645d0ac515
oliversolman <30669421+oliversolman@users.noreply.github.com>
2021-05-09 11:58:08 -0700

    Add files via upload

---
+++ WVD/donot-askforupdate-wvd-alt.ps1
@@ -0,0 +1,11 @@
+﻿$condition = $true
+if (!(Test-Path -Path hklm:\SOFTWARE\Microsoft\msrdc)){
+#New-Item -Path "HKLM:\SOFTWARE\Microsoft\msrdc\"
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\msrdc\Policies\" -Force
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\msrdc\Policies\AutomaticUpdates"
+New-ItemProperty -Name AutomaticUpdates -PropertyType dword -Path HKLM:\SOFTWARE\Microsoft\msrdc\Policies\ -Value 0
+}
+else{
+Write-Host "friend, it is already there"
+}
+
---
+++ WVD/donot-askforupdate-wvd.ps1
@@ -0,0 +1,11 @@
+﻿$condition = $true
+if (Test-Path -Path hklm:\SOFTWARE\Microsoft\msrdc){
+Write-Host "already there"
+}
+else{
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\msrdc\"
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\msrdc\Policies\"
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\msrdc\Policies\AutomaticUpdates"
+New-ItemProperty -Name AutomaticUpdates -PropertyType dword -Path HKLM:\SOFTWARE\Microsoft\msrdc\Policies\ -Value 0
+}
+
